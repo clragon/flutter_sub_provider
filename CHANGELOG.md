9a96469b208b0d53d407e077d2e279386522f16d
dragon <kenta3ds.habbo@gmail.com>
2023-03-19 15:51:54 +0100

    fixed developer library

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0
+
+- Fix developer library
+
 ## 0.1.0
 
 - Published flutter_sub_provider
--- lib/developer.dart
+++ lib/developer.dart
@@ -1,7 +1,7 @@
 /// This library can be used to create custom [SubProvider]s.
 ///
 /// It allows access to function types and [SingleChildWidget].
-library flutter_sub_provider;
+library developer;
 
 import 'package:flutter_sub_provider/src/sub_provider.dart';
 import 'package:provider/single_child_widget.dart';
